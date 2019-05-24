6c2b51c5086e9e8eda09f80839082dfd4544df61
mmomjian <50788000+mmomjian@users.noreply.github.com>
2019-05-24 14:06:52 -0400

    Update geo.js

--- js/geo.js
+++ js/geo.js
@@ -1,53 +1,26 @@
-// geo.js
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+	var myLatlng = new google.maps.LatLng(15, 11);
 
-// infobox_packed.js
-
-// http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/docs/reference.html
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 8(a){a=a||{};9.p.1O.2h(2,33);2.M=a.1s||"";2.1A=a.1n||G;2.Y=a.1F||0;2.E=a.1y||1e 9.p.1V(0,0);2.z=a.X||1e 9.p.2x(0,0);2.T=a.S||t;2.1k=a.1j||"2d";2.1i=a.D||{};2.1C=a.1B||"35";2.K=a.1g||"31://2W.9.2Q/2J/2I/2G/1v.2D";3(a.1g===""){2.K=""}2.17=a.1x||1e 9.p.1V(1,1);2.V=a.1o||G;2.16=a.1m||G;2.1J=a.2j||"2g";2.14=a.1q||G;2.4=t;2.w=t;2.P=t;2.O=t;2.B=t;2.N=t}8.q=1e 9.p.1O();8.q.24=6(){5 i;5 f;5 a;5 d=2;5 c=6(e){e.21=Z;3(e.15){e.15()}};5 b=6(e){e.2Z=G;3(e.1Y){e.1Y()}3(!d.14){c(e)}};3(!2.4){2.4=1f.2P("2M");2.1d();3(s 2.M.1r==="r"){2.4.L=2.F()+2.M}v{2.4.L=2.F();2.4.1a(2.M)}2.2C()[2.1J].1a(2.4);2.1z();3(2.4.7.C){2.N=Z}v{3(2.Y!==0&&2.4.W>2.Y){2.4.7.C=2.Y;2.4.7.2z="2w";2.N=Z}v{a=2.1N();2.4.7.C=(2.4.W-a.Q-a.13)+"12";2.N=G}}2.1p(2.1A);3(!2.14){2.B=[];f=["2p","1L","2o","2n","1K","2m","2l","2k","2i"];1l(i=0;i<f.1I;i++){2.B.1H(9.p.u.19(2.4,f[i],c))}2.B.1H(9.p.u.19(2.4,"1L",6(e){2.7.1G="2f"}))}2.O=9.p.u.19(2.4,"2e",b);9.p.u.R(2,"2c")}};8.q.F=6(){5 a="";3(2.K!==""){a="<2b";a+=" 2a=\'"+2.K+"\'";a+=" 29=13";a+=" 7=\'";a+=" X: 28;";a+=" 1G: 27;";a+=" 26: "+2.1C+";";a+="\'>"}J a};8.q.1z=6(){5 a;3(2.K!==""){a=2.4.3d;2.w=9.p.u.19(a,\'1K\',2.25())}v{2.w=t}};8.q.25=6(){5 a=2;J 6(e){e.21=Z;3(e.15){e.15()}9.p.u.R(a,"3c");a.1v()}};8.q.1p=6(d){5 m;5 n;5 e=0,H=0;3(!d){m=2.3a();3(m 39 9.p.38){3(!m.23().37(2.z)){m.36(2.z)}n=m.23();5 a=m.34();5 h=a.W;5 f=a.22;5 k=2.E.C;5 l=2.E.1h;5 g=2.4.W;5 b=2.4.22;5 i=2.17.C;5 j=2.17.1h;5 o=2.20().32(2.z);3(o.x<(-k+i)){e=o.x+k-i}v 3((o.x+g+k+i)>h){e=o.x+g+k+i-h}3(2.16){3(o.y<(-l+j+b)){H=o.y+l-j-b}v 3((o.y+l+j)>f){H=o.y+l+j-f}}v{3(o.y<(-l+j)){H=o.y+l-j}v 3((o.y+b+l+j)>f){H=o.y+b+l+j-f}}3(!(e===0&&H===0)){5 c=m.30();m.2Y(e,H)}}}};8.q.1d=6(){5 i,D;3(2.4){2.4.2X=2.1k;2.4.7.2V="";D=2.1i;1l(i 2U D){3(D.2R(i)){2.4.7[i]=D[i]}}3(s 2.4.7.18!=="r"&&2.4.7.18!==""){2.4.7.2O="2N(18="+(2.4.7.18*2L)+")"}2.4.7.X="2K";2.4.7.11=\'1u\';3(2.T!==t){2.4.7.S=2.T}}};8.q.1N=6(){5 c;5 a={1c:0,1b:0,Q:0,13:0};5 b=2.4;3(1f.1t&&1f.1t.1W){c=b.2H.1t.1W(b,"");3(c){a.1c=A(c.1U,10)||0;a.1b=A(c.1T,10)||0;a.Q=A(c.1X,10)||0;a.13=A(c.1S,10)||0}}v 3(1f.2F.I){3(b.I){a.1c=A(b.I.1U,10)||0;a.1b=A(b.I.1T,10)||0;a.Q=A(b.I.1X,10)||0;a.13=A(b.I.1S,10)||0}}J a};8.q.2E=6(){3(2.4){2.4.2S.2T(2.4);2.4=t}};8.q.1E=6(){2.24();5 a=2.20().2B(2.z);2.4.7.Q=(a.x+2.E.C)+"12";3(2.16){2.4.7.1b=-(a.y+2.E.1h)+"12"}v{2.4.7.1c=(a.y+2.E.1h)+"12"}3(2.V){2.4.7.11=\'1u\'}v{2.4.7.11="1R"}};8.q.2A=6(a){3(s a.1j!=="r"){2.1k=a.1j;2.1d()}3(s a.D!=="r"){2.1i=a.D;2.1d()}3(s a.1s!=="r"){2.1Q(a.1s)}3(s a.1n!=="r"){2.1A=a.1n}3(s a.1F!=="r"){2.Y=a.1F}3(s a.1y!=="r"){2.E=a.1y}3(s a.1m!=="r"){2.16=a.1m}3(s a.X!=="r"){2.1w(a.X)}3(s a.S!=="r"){2.1P(a.S)}3(s a.1B!=="r"){2.1C=a.1B}3(s a.1g!=="r"){2.K=a.1g}3(s a.1x!=="r"){2.17=a.1x}3(s a.1o!=="r"){2.V=a.1o}3(s a.1q!=="r"){2.14=a.1q}3(2.4){2.1E()}};8.q.1Q=6(a){2.M=a;3(2.4){3(2.w){9.p.u.U(2.w);2.w=t}3(!2.N){2.4.7.C=""}3(s a.1r==="r"){2.4.L=2.F()+a}v{2.4.L=2.F();2.4.1a(a)}3(!2.N){2.4.7.C=2.4.W+"12";3(s a.1r==="r"){2.4.L=2.F()+a}v{2.4.L=2.F();2.4.1a(a)}}2.1z()}9.p.u.R(2,"2y")};8.q.1w=6(a){2.z=a;3(2.4){2.1E()}9.p.u.R(2,"1Z")};8.q.1P=6(a){2.T=a;3(2.4){2.4.7.S=a}9.p.u.R(2,"2v")};8.q.2u=6(){J 2.M};8.q.1D=6(){J 2.z};8.q.2t=6(){J 2.T};8.q.2s=6(){2.V=G;3(2.4){2.4.7.11="1R"}};8.q.2r=6(){2.V=Z;3(2.4){2.4.7.11="1u"}};8.q.2q=6(c,b){5 a=2;3(b){2.z=b.1D();2.P=9.p.u.3b(b,"1Z",6(){a.1w(2.1D())})}2.1M(c);3(2.4){2.1p()}};8.q.1v=6(){5 i;3(2.w){9.p.u.U(2.w);2.w=t}3(2.B){1l(i=0;i<2.B.1I;i++){9.p.u.U(2.B[i])}2.B=t}3(2.P){9.p.u.U(2.P);2.P=t}3(2.O){9.p.u.U(2.O);2.O=t}2.1M(t)};',62,200,'||this|if|div_|var|function|style|InfoBox|google||||||||||||||||maps|prototype|undefined|typeof|null|event|else|closeListener_|||position_|parseInt|eventListeners_|width|boxStyle|pixelOffset_|getCloseBoxImg_|false|yOffset|currentStyle|return|closeBoxURL_|innerHTML|content_|fixedWidthSet_|contextListener_|moveListener_|left|trigger|zIndex|zIndex_|removeListener|isHidden_|offsetWidth|position|maxWidth_|true||visibility|px|right|enableEventPropagation_|stopPropagation|alignBottom_|infoBoxClearance_|opacity|addDomListener|appendChild|bottom|top|setBoxStyle_|new|document|closeBoxURL|height|boxStyle_|boxClass|boxClass_|for|alignBottom|disableAutoPan|isHidden|panBox_|enableEventPropagation|nodeType|content|defaultView|hidden|close|setPosition|infoBoxClearance|pixelOffset|addClickHandler_|disableAutoPan_|closeBoxMargin|closeBoxMargin_|getPosition|draw|maxWidth|cursor|push|length|pane_|click|mouseover|setMap|getBoxWidths_|OverlayView|setZIndex|setContent|visible|borderRightWidth|borderBottomWidth|borderTopWidth|Size|getComputedStyle|borderLeftWidth|preventDefault|position_changed|getProjection|cancelBubble|offsetHeight|getBounds|createInfoBoxDiv_|getCloseClickHandler_|margin|pointer|relative|align|src|img|domready|infoBox|contextmenu|default|floatPane|apply|touchmove|pane|touchend|touchstart|dblclick|mouseup|mouseout|mousedown|open|hide|show|getZIndex|getContent|zindex_changed|auto|LatLng|content_changed|overflow|setOptions|fromLatLngToDivPixel|getPanes|gif|onRemove|documentElement|mapfiles|ownerDocument|en_us|intl|absolute|100|div|alpha|filter|createElement|com|hasOwnProperty|parentNode|removeChild|in|cssText|www|className|panBy|returnValue|getCenter|http|fromLatLngToContainerPixel|arguments|getDiv|2px|setCenter|contains|Map|instanceof|getMap|addListener|closeclick|firstChild'.split('|'),0,{}))
-
-// google_map_api.js
-
-// Display Info Windows Above Markers
-
-addEvent(window, 'load', display_map);
-
-function display_map() {
-    var is_both = (location.search == '?map_type=both') ? 1 : 0;
-
-    // Use longitude so map is continent-aligned on start
-    // Align vertically on Tropic of Cancer
-//    var myLatlng = new google.maps.LatLng(23.43716, 11);
-    var myLatlng = new google.maps.LatLng(15, 11);
-
-    var myOptions = {
-      zoom: 2,
-      center: myLatlng,
-      streetViewControl: false,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var map = new google.maps.Map(document.getElementById("map"), myOptions);
-
-    // image is 12x20
-    var marker_img_p = new google.maps.MarkerImage('/main/img/mm_20_p.png');
-    var marker_img_f = new google.maps.MarkerImage('/main/img/mm_20_f.png');
-    // offset -9
-    var marker_img_f_offset = new google.maps.MarkerImage('/main/img/mm_20_f.png',
-	null, null, new google.maps.Point(-3,20), null);
-
-    // http://stackoverflow.com/questions/7842730/change-marker-size-in-google-maps-v3
-    // shadow image size is 22x20
-    var marker_shadow = new google.maps.MarkerImage('/main/img/mm_20_shadow.png',
-	null, null, new google.maps.Point(7,20), null);
-    // offset -9
-    var marker_shadow_offset = new google.maps.MarkerImage('/main/img/mm_20_shadow.png',
-	null, null, new google.maps.Point(-2,20), null);
-
-    // offset of info box from marker
-    var info_offset = new google.maps.Size(10, -15);
-
-    // Creates a marker whose info window displays the given location string
-    function createMarker(map_pins, lat, lng, locstr) {
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 2, center: myLatlng});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRj4uKuheL_y9U6Nb8uhQS4HGF0779XOs&callback=initMap">
+    </script>
+	    <script type="text/javascript">
+		        function createMarker(map_pins, lat, lng, locstr) {
       // create point
       var latlng = new google.maps.LatLng(lat, lng)
 
@@ -56,9 +29,6 @@ function display_map() {
         position: latlng,
         map: map,
         // in 'both' mode, display family pins offset and below
-        icon: marker_img_p,
-        shadow: marker_shadow,
-	zIndex: map_pins == matthew_momjian_geo_locations ? 2 : 1
       });
 
       // create info box
@@ -67,7 +37,6 @@ function display_map() {
         disableAutoPan: true,
         closeBoxURL: '',
         enableEventPropagation: true, // used for map labels
-        pixelOffset: info_offset
       });
 
       // Show this marker's description when the mouse is over it
@@ -100,6 +69,4 @@ function display_map() {
 	}
     }
 
-	addMapPins(matthew_momjian_geo_locations, "Travel");
-
-}
+		    addMapPins(matthew_momjian_geo_locations, "Travel");</script>
