bfd764f185c8b761eb5d426d38ee2f7b1a2528e9
mmomjian <50788000+mmomjian@users.noreply.github.com>
2025-03-07 17:30:47 -0500

    f

--- js/gapi2.js
+++ js/gapi2.js
@@ -23,9 +23,6 @@ async function initMap() {
   matthew_momjian_geo_locations.forEach((location) => {
     const [country, city, years, lat, lng] = location;
     var printlocation = city ? `${city}, ${country}` : country;
-    var info_offset = new google.maps.Size(10, -15);
-
-    var close_timeout = null, close_func, in_mouseover = false;
 
     const marker = new AdvancedMarkerElement({
       map: map,
@@ -40,43 +37,9 @@ async function initMap() {
         disableAutoPan: true,
         closeBoxURL: '',
         enableEventPropagation: true, // used for map labels
-        pixelOffset: info_offset
+//        pixelOffset: info_offset
       });
 
-      // Show this marker's description when the mouse is over it
-      google.maps.event.addListener(marker, 'mouseover', function() {
-        infowindow.open(map, marker);
-        in_mouseover = true;
-      });
-      google.maps.event.addListener(marker, 'mouseout', function() {
-        infowindow.close(map, marker);
-        in_mouseover = false;
-      });
-
-      // For touch screens
-      google.maps.event.addListener(marker, 'mousedown', function() {
-        // don't use click timer in mouse over
-        if (!in_mouseover)
-        {
-            // close an open window?
-            if (close_timeout !== null)
-            {
-	        // must be first
-	        clearTimeout(close_timeout);
-	        close_func();
-            }
-            infowindow.open(map, marker);
-         }
-      });
-      google.maps.event.addListener(marker, 'mouseup', function() {
-        if (!in_mouseover)
-        {
-            // give time to view the text once finger is removed
-            close_func = function() { close_timeout = null; infowindow.close(map, marker); };
-
-            close_timeout = setTimeout(close_func, 4000);
-        }
-      });
 
 
 
