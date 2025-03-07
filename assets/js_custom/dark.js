7d8f7faaeef62b3de8706807d43f20ad347b9bf2
mmomjian <50788000+mmomjian@users.noreply.github.com>
2025-03-07 18:56:12 -0500

    light dark text

--- assets/js_custom/dark.js
+++ assets/js_custom/dark.js
@@ -1,27 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const toggleButton = document.getElementById("nav-dark-link");
     const icon = document.getElementById("nav-dark-icon");
     const currentTheme = localStorage.getItem("theme");
+    const text = document.getElementById("nav-dark-text");
+
+    function makeDark () {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun"); // Show sun icon when dark mode is active
+        text.innerHTML = "Light mode";
+ return; }
+
+    function makeLight () {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon"); // Show moon icon when light mode is active
+        text.innerHTML = "Dark mode";
+ return; }
 
     if (currentTheme === "dark") {
         document.body.classList.add("dark-mode");
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-sun"); // Show sun icon when dark mode is active
+        makeDark();
     } else {
-        icon.classList.remove("fa-sun");
-        icon.classList.add("fa-moon"); // Show moon icon when light mode is active
+        makeLight();
     }
 
     toggleButton.addEventListener("click", function () {
         document.body.classList.toggle("dark-mode");
         if (document.body.classList.contains("dark-mode")) {
             localStorage.setItem("theme", "dark");
-            icon.classList.remove("fa-moon");
-            icon.classList.add("fa-sun"); // Change icon to sun in dark mode
+            makeDark();
         } else {
             localStorage.setItem("theme", "light");
-            icon.classList.remove("fa-sun");
-            icon.classList.add("fa-moon"); // Change icon back to moon in light mode
+            makeLight();
         }
     });
 });
